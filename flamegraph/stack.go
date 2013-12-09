ee2340224db00285ca37c098a57256b510884211
Kamil Kisiel <kamil@kamilkisiel.net>
2013-12-08 23:33:24 -0800

    Merge foldstack.go and stack.go

--- flamegraph/stack.go
+++ flamegraph/stack.go
@@ -5,6 +5,8 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"sort"
+	"strconv"
 	"strings"
 )
 
@@ -64,4 +66,28 @@ func ParseStacks(r io.Reader) ([]*Stack, error) {
 		}
 	}
 	return stacks, nil
+}
+
+func foldStack(s Stack) string {
+	calls := make([]string, len(s.Calls))
+	for i, c := range s.Calls {
+		calls[i] = c.Source + "`" + c.Func
+	}
+	return strings.Join(calls, ";")
+}
+
+func FoldStacks(stacks []*Stack) []string {
+	counts := make(map[string]int)
+	for _, s := range stacks {
+		folded := foldStack(*s)
+		counts[folded]++
+	}
+
+	lines := make([]string, 0, len(counts))
+	for line, count := range counts {
+		lines = append(lines, line+" "+strconv.Itoa(count))
+	}
+
+	sort.Strings(lines)
+	return lines
 }
